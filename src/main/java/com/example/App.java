2054492cd8a81aad2b3dd108bb349a2983d9949f
Shubham Suresh Chavan <shubhamchav@cybage.com>
2025-02-24 15:42:26 +0530

    project added

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class App {
+   public int add(int a, int b) {
+       return a + b;
+   }
+   public static void main(String[] args) {
+       App app = new App();
+       System.out.println("Sum: " + app.add(1, 2));
+       System.out.println("This is a test");
+       System.out.println("Adding duplications");
+       System.out.println("Adding duplications");
+       System.out.println("Adding duplications");
+       System.out.println("Adding duplications");
+  }
+}
